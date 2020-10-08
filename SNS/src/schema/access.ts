0b47bf19f68f5839ab55a27c2eb290870ae61ee2
yu myeong cheol <mspringmspring@naver.com>
2020-10-08 23:27:33 +0900

    [MODIFY] <schema/access.ts>

--- SNS/src/schema/access.ts
+++ SNS/src/schema/access.ts
@@ -6,8 +6,8 @@ export default {
     password: Joi.string().required().min(6),
   }),
   signup: Joi.object().keys({
-    name: Joi.string().required().min(3),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
+    nickname: Joi.string().required().min(3),
   }),
 };
