f0a3e65f08128d910a0acf96a0b0f2549c0801d6
yu myeong cheol <mspringmspring@naver.com>
2020-10-08 23:26:06 +0900

    [MODIFY] <repository/UserRepo.ts> logic

--- SNS/src/database/repository/UserRepo.ts
+++ SNS/src/database/repository/UserRepo.ts
@@ -1,17 +1,23 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../model/User';
 
-@EntityRepository()
-class UserRepository extends Repository<User> {
-  async findByEmail(email: string) {
+@EntityRepository(User)
+export default class UserRepository extends Repository<User> {
+  public async findByEmail(email: string): Promise<User> {
     return await this.createQueryBuilder('user')
       .where('user.email = :email', { email })
       .getOne();
   }
 
-  //   async create() {
-
-  //   };
+  public async createUser(
+    email: string,
+    password: string,
+    nickname: string,
+  ): Promise<User> {
+    const user = new User();
+    user.email = email;
+    user.password = password;
+    user.nickname = nickname;
+    return await this.manager.save(user);
+  }
 }
-
-export default new UserRepository();
