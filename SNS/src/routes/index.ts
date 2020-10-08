0e5cd2455abb79f340807520a36f1fb4ab9671b6
yu myeong cheol <mspringmspring@naver.com>
2020-10-08 23:26:49 +0900

    [ADD] <routes/index.ts> router

--- SNS/src/routes/index.ts
+++ SNS/src/routes/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import apikey from '../auth/apikey';
+import signup from './access/signup';
 
 const router = express.Router();
+
+router.use('/', apikey);
+
+router.use('/access', signup);
 
 export default router;
