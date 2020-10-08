98843fae1ab7f1d3601de30c21e9e3b27597fdf9
yu myeong cheol <mspringmspring@naver.com>
2020-10-08 23:27:46 +0900

    [DELETE] <src/server.ts>

--- SNS/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { port } from './config';
-import Logger from './core/Logger';
-import app from './app';
-
-app
-  .listen(80, () => {
-    Logger.info(`server running on port : ${port}`);
-  })
-  .on('error', (e) => console.log(e));
