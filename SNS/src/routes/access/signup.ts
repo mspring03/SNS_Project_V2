c05acba95d7bb7fdc172e52ebc174bf3e0c18613
yu myeong cheol <mspringmspring@naver.com>
2020-10-10 23:32:43 +0900

    [MODIFY] <access/signup.ts>

--- SNS/src/routes/access/signup.ts
+++ SNS/src/routes/access/signup.ts
@@ -1,53 +1,47 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import { SuccessResponse } from '../../core/ApiResponse';
 import validator from '../../middleware/validator';
 import schema from '../../schema/access';
 import asyncHandler from '../../middleware/asyncHandler';
 import UserRepo from '../../database/repository/UserRepo';
+import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 import _ from 'lodash';
 import { getCustomRepository } from 'typeorm';
 import sender from '../../middleware/emailSender';
-import {
-  BadRequestError,
-  AuthFailureError,
-  NotFoundError,
-} from '../../core/ApiError';
+import { hostAddress } from '../../config';
+import { BadRequestError, NotFoundError } from '../../core/ApiError';
+import LoginKeyRepository from '../../database/repository/loginKeyRepo';
 
 const router = express.Router();
 
 router.post(
   '/signup',
   validator(schema.signup),
-  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  asyncHandler(async (req: Request, res: Response) => {
     const userRepo = getCustomRepository(UserRepo);
+    const loginKeyRepository = getCustomRepository(LoginKeyRepository);
     const user = await userRepo.findByEmail(req.body.email);
 
     if (user) throw new BadRequestError('User already registered');
 
     const passwordHash = await bcrypt.hash(req.body.password, 10);
+    const loginAccessKey = crypto.randomBytes(17).toString('hex');
 
+    const url = `<div style="width: 1100px; height: 50px; background-color: #99ccff;"><a style="text-align: center" href="${hostAddress}/v2/access/signup/?email=${req.body.email}&password=${passwordHash}&nickname=${req.body.nickname}">${hostAddress}/v2/access/signup/?email=${req.body.email}&password=${passwordHash}&nickname=${req.body.nickname}&key=${loginAccessKey}</a></div>`;
     const mailOptions = {
-      from: 'mspringmspring@naver.com',
+      from: 'aespringaa@gmail.com',
       to: req.body.email,
       subject: 'E-mail confirmation url',
-      text: 'ddd',
-      html: `<div style="width: 100px; height: 10px; border: 1 solid #99ccff;"><a href="170.0.0.1:80/?email=${req.body.email}&password=${passwordHash}&nickname=${req.body.nickname}"></a></div>`, // 내용
+      html: url,
     };
 
-    const errorMessage = await sender(mailOptions);
-    console.log(errorMessage);
+    const errorMessage: string = await sender(mailOptions);
+    if (errorMessage) throw new NotFoundError(errorMessage);
 
-    // throw new NotFoundError(errorMessage);
-
-    // const createdUser: User = await userRepo.createUser(
-    //   req.body.email,
-    //   passwordHash,
-    //   req.body.nickname,
-    // );
+    await loginKeyRepository.createKey(loginAccessKey);
 
     new SuccessResponse('send Successful', {
-      // user: _.pick(createdUser, ['_id', 'email', 'nickname']),
       message: 'Send mail',
     }).send(res);
   }),
