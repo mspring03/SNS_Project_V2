10237d75c42a2d0924e15a5694952d5adafde522
yu myeong cheol <mspringmspring@naver.com>
2020-10-10 23:33:09 +0900

    [CREATE] <access/signupCallback.ts> signup callback

---
+++ SNS/src/routes/access/signupCallback.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import asyncHandler from '../../middleware/asyncHandler';
+import { SuccessResponse } from '../../core/ApiResponse';
+import UserRepo from '../../database/repository/UserRepo';
+import { getCustomRepository } from 'typeorm';
+import { User } from '../../database/model/User';
+import userQueryData from '../../types/app-request';
+import _ from 'lodash';
+import url from 'url';
+
+const router = express.Router();
+
+router.post(
+  '/callback',
+  asyncHandler(async (req: Request, res: Response) => {
+    const userRepo = getCustomRepository(UserRepo);
+    const queryData = url.parse(req.url, true).query;
+
+    const createdUser: User = await userRepo.createUser(
+      String(queryData.email),
+      String(queryData.password),
+      String(queryData.nickname),
+    );
+
+    new SuccessResponse('create Successful', {
+      user: _.pick(createdUser, ['_id', 'email', 'nickname']),
+    }).send(res);
+  }),
+);
