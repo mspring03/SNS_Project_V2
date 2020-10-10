e4b956deb196b9ed13db238967cf4847efe1ef12
yu myeong cheol <mspringmspring@naver.com>
2020-10-10 23:32:20 +0900

    [MODIFY] <middleware/emailSender.ts>

--- SNS/src/middleware/emailSender.ts
+++ SNS/src/middleware/emailSender.ts
@@ -1,29 +1,23 @@
-// import nodemailer from 'nodemailer';
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
+// const nodemailer = require('nodemailer');
 import { NextFunction } from 'express';
 import { NotFoundError } from '../core/apiError';
 import Logger from '../core/Logger';
 
 let transporter = nodemailer.createTransport({
-  service: 'naver',
-  host: 'smtp.naver.com',
-  port: 587,
+  service: 'gmail',
   auth: {
-    user: 'mspringmspring@naver.com', // gmail 계정 아이디를 입력
-    pass: '@@aa030517', // gmail 계정의 비밀번호를 입력
-  },
-  tls: {
-    rejectUnauthorized: false,
+    user: 'aespringaa@gmail.com', // gmail 계정 아이디를 입력
+    pass: 'bazvzoqopkodfhxo', // gmail 계정의 비밀번호를 입력
   },
 });
 
-const sender = (mailOptions: object) => {
+const sender = (mailOptions: object): string => {
   transporter.sendMail(mailOptions, function (error: Error) {
-    console.log(error.message);
-
-    if (error) return error;
+    if (error) return error.message;
     Logger.info('Email sent');
   });
+  return '';
 };
 
 export default sender;
