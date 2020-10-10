694b0a809e4759c21c3cfcf21b527df3330399f8
yu myeong cheol <mspringmspring@naver.com>
2020-10-10 23:33:38 +0900

    [MODIFY] <auth/apikey.ts>

--- SNS/src/auth/apikey.ts
+++ SNS/src/auth/apikey.ts
@@ -6,17 +6,31 @@ import validator, { ValidationSource } from '../middleware/validator';
 import asyncHandler from '../middleware/asyncHandler';
 import { apiKey } from '../config';
 import Logger from '../core/Logger';
+import LoginKeyRepository from '../database/repository/loginKeyRepo';
+import { getCustomRepository } from 'typeorm';
+import url from 'url';
 
 const router = express.Router();
 
 export default router.use(
   validator(schema.apiKey, ValidationSource.HEADER),
   asyncHandler(async (req: PublicRequest, res, next) => {
+    const loginKeyRepository = getCustomRepository(LoginKeyRepository);
+    const queryData = url.parse(req.url, true).query;
+
     req.apiKey = req.headers['x-api-key'].toString();
 
-    if (req.apiKey !== apiKey) throw new ForbiddenError();
-    Logger.info(apiKey);
+    if (req.apiKey === apiKey) {
+      Logger.info(apiKey);
+      return next();
+    }
+    console.log(queryData.key);
 
-    return next();
+    if (await loginKeyRepository.findKey(String(queryData.key))) {
+      Logger.info(apiKey);
+      return next();
+    }
+
+    throw new ForbiddenError();
   }),
 );
