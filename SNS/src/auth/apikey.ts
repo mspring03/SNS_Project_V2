d2d3138a8bf6607d7516a1f1926f5332c024f782
yu myeong cheol <mspringmspring@naver.com>
2020-10-11 17:09:57 +0900

    [MODIFY] <auth/apikey.ts> remove validator

--- SNS/src/auth/apikey.ts
+++ SNS/src/auth/apikey.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import { ForbiddenError } from '../core/ApiError';
 import { PublicRequest } from 'app-request';
-import schema from '../schema/auth';
-import validator, { ValidationSource } from '../middleware/validator';
 import asyncHandler from '../middleware/asyncHandler';
 import { apiKey } from '../config';
 import Logger from '../core/Logger';
@@ -13,21 +11,17 @@ import url from 'url';
 const router = express.Router();
 
 export default router.use(
-  validator(schema.apiKey, ValidationSource.HEADER),
   asyncHandler(async (req: PublicRequest, res, next) => {
     const loginKeyRepository = getCustomRepository(LoginKeyRepository);
     const queryData = url.parse(req.url, true).query;
 
-    req.apiKey = req.headers['x-api-key'].toString();
-
-    if (req.apiKey === apiKey) {
+    if (String(req.headers['x-api-key']) === apiKey) {
       Logger.info(apiKey);
       return next();
     }
-    console.log(queryData.key);
 
     if (await loginKeyRepository.findKey(String(queryData.key))) {
-      Logger.info(apiKey);
+      await loginKeyRepository.removeKey(String(queryData.key));
       return next();
     }
 
