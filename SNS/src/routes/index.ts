2e042c2725d756e998ea966ae2c11c03886fc054
yu myeong cheol <mspringmspring@naver.com>
2020-10-11 17:13:17 +0900

    [ADD] <routes/index.ts> signupCallback router

--- SNS/src/routes/index.ts
+++ SNS/src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import apikey from '../auth/apikey';
 import signup from './access/signup';
+import signupCallback from './access/signupCallback';
 
 const router = express.Router();
 
 router.use('/', apikey);
 
-router.use('/access', signup);
+router.use('/signup', signup);
+router.use('/signupCallback', signupCallback);
 
 export default router;
