1a7d7cb31807542df4a2be05a6729da58334014d
yu myeong cheol <mspringmspring@naver.com>
2020-10-11 17:10:24 +0900

    [MODIFY] <model/loginKey.ts>

--- SNS/src/database/model/loginKey.ts
+++ SNS/src/database/model/loginKey.ts
@@ -1,8 +1,8 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class loginKey {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   _id: number;
 
   @Column()
